f29e219f2e68fd057b4d8270d263c4d7a2fc636d
misho <mishodzuliashvili1@gmail.com>
2022-07-15 20:48:55 +0400

    footer added

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import "./Footer.css";
+function Footer() {
+	return (
+		<div class="footer">
+            <h1>HardestWebsite</h1>
+            <br />
+            <p>Hardest Website is our heart. we are zuka, luka and misho. very curious people who love coding.</p>
+            <br />
+            <a style={{color:"yellow"}} href="google.com">Lets speak. FaceBook</a>
+            <br />
+		</div>
+	);
+}
+
+export default Footer;
