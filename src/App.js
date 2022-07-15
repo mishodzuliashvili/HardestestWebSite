52dc2ec4a17da3a9a049b5b8c132b4c7efe57dc1
misho <mishodzuliashvili1@gmail.com>
2022-07-15 20:14:49 +0400

    yeahhh

--- src/App.js
+++ src/App.js
@@ -1,11 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
-const high = (fun) => {
-  fun("gela")
-}
-const auxF = res => console.log(res)
-high(auxF);
-
 function App() {
   return (
     <h1>{0 === 0 ? <p>zz</p> : <p>hey</p>}</h1>
